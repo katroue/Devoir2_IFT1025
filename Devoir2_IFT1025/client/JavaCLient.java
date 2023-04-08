2ff1e61bbc3439dbf1e2be9c49eb32d163dc0237
katroue <122406701+katroue@users.noreply.github.com>
2023-04-08 12:23:53 -0400

    Update JavaCLient.java

--- Devoir2_IFT1025/client/JavaCLient.java
+++ Devoir2_IFT1025/client/JavaCLient.java
@@ -4,9 +4,25 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class main(String [] args) {
-    Socket clientSocket = new Socket("127.0.0.1", 1337);
-
-    public void getAvailableCourses() {
-
+    try {Socket clientSocket = new Socket("127.0.0.1", 1337);
+    
+         OutputStreamWriter os = new OutputStreamWriter(clientSocket.getOutputStream());
+    
+         BufferedWirter writer = new BufferedWriter(os);
+    
+         Scanner scanner = new Scanner(System.in);
+    
+         while(scanner.hasNext()) {
+             String line = scanner.nextLine();
+             System.out.println(line);
+        
+             writer.flush();
+         }
+         
+         wirter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
+    
 }
